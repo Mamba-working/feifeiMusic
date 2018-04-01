e625b7b21eb7036cc6146da509dd5e68766106e6
wufeifei <1162374744@qq.com>
2018-04-01 11:06:47 +0800

    demo

--- js/music.js
+++ js/music.js
@@ -278,9 +278,15 @@ let Fm = {
         let img = this.song.picture || this.song.album.picUrl;
         this.music.src = this.song.url || ("http://music.163.com/song/media/outer/url?id="+this.song.id+".mp3");
         $(".background").css("background-image", "url" + '(' + img + ')');
-        $("main section h1").text(this.song.title || this.song.name);
-        $(".author").text(this.song.artist);
-        $("main section>.cat").text(this.channel_name);
+        $("main section h1").fadeOut("slow", () =>{
+            $("main section h1").text(this.song.title || this.song.name).fadeIn("slow")
+        });
+        $(".author").fadeOut("slow",()=>{
+            $(".author").text(this.song.artist || this.song.artists[0].name).fadeIn();
+        })
+        $("main section>.cat").fadeOut("slow",()=>{
+            $("main section>.cat").text(this.channel_name || this.song.album.name).fadeIn("slow");
+        })
         $(".aside>figure").css("background-image", "url" + '(' + img + ')')
     },
     loadLyric() {
